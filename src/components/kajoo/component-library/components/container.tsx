dd3ecf9a4f4be3bd58e44eff04e3505e0b7f0988
Guvanch G <guvanch.gurbandurdyyev@techguilds.com>
2024-08-28 23:32:51 -0400

    fix bg image

--- src/components/kajoo/component-library/components/container.tsx
+++ src/components/kajoo/component-library/components/container.tsx
@@ -1,8 +1,9 @@
 import { FC, ComponentType, ReactNode } from "react";
 import { toObject } from "../toObject";
+import { LinkDataFragment, ReferenceDataFragment } from "@/gql/graphql";
 
 export interface IProps {
-  bgImage?: { value: { src: string } };
+  bgImage?: LinkDataFragment | ReferenceDataFragment | null;
   tag?: keyof JSX.IntrinsicElements | ComponentType;
   [key: string]: any;
   children: ReactNode | ReactNode[];
@@ -15,11 +16,12 @@ const Container: FC<IProps> = ({
   children,
   ...rest
 }) => {
-  const backgroundImage = bgImage?.value?.src || bgImage?.value || bgImage;
+  const link = linkDataToUrl(getLinkData(bgImage));
+  const imgSrc = link?.href;
   const styleObject = toObject(style);
   const containerStyle = {
     ...styleObject,
-    ...(backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : {}),
+    ...(imgSrc ? { backgroundImage: `url(${imgSrc})` } : {}),
   };
 
   const Tag: keyof JSX.IntrinsicElements = tag as keyof JSX.IntrinsicElements;
@@ -32,3 +34,22 @@ const Container: FC<IProps> = ({
 };
 
 export default Container;
+
+function getLinkData(
+  input?: LinkDataFragment | ReferenceDataFragment | null
+): LinkDataFragment | undefined {
+  if (!input) return undefined;
+  if ((input as ReferenceDataFragment).url)
+    return (input as ReferenceDataFragment).url ?? undefined;
+  return (input as LinkDataFragment) ?? undefined;
+}
+
+function linkDataToUrl(
+  item: LinkDataFragment | null | undefined
+): URL | undefined {
+  try {
+    return new URL(item?.default ?? "/", item?.base ?? undefined);
+  } catch {
+    return undefined;
+  }
+}
