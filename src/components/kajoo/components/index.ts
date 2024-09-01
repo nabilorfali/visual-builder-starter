f9c04235022da6535509b59ad36308d10cc7cc10
Guvanch G <guvanch.gurbandurdyyev@techguilds.com>
2024-08-31 21:30:39 -0400

    cleanup

--- src/components/kajoo/components/index.ts
+++ src/components/kajoo/components/index.ts
@@ -1,9 +1,59 @@
+import HeroBanner from "./HeroBanner";
+import MainNavigation from "./MainNavigation";
+import DifferentorPanel from "./DifferentorPanel";
+import BackgroundDifferentiatorPanel from "./BackgroundDifferentiatorPanel";
+import IntroOneColumn from "./IntroOneColumn";
+import ReverseDifferentorPanel from "./ReverseDifferentorPanel";
 
+const kajooComponentDictionary = [
+  {
+    type: "Component/Element/HeroBannerElement",
+    component: HeroBanner,
+  },
+  {
+    type: "Component/HeroBannerBlock",
+    component: HeroBanner,
+  },
+  {
+    type: "Component/Element/MainNavigationElement",
+    component: MainNavigation,
+  },
+  {
+    type: "Component/MainNavigationBlock",
+    component: MainNavigation,
+  },
+  {
+    type: "Component/Element/DifferentorPanelElement",
+    component: DifferentorPanel,
+  },
+  {
+    type: "Component/DifferentorPanelBlock",
+    component: DifferentorPanel,
+  },
+  {
+    type: "Component/Element/BackgroundDifferentiatorPanelElement",
+    component: BackgroundDifferentiatorPanel,
+  },
+  {
+    type: "Component/BackgroundDifferentiatorPanelBlock",
+    component: BackgroundDifferentiatorPanel,
+  },
+  {
+    type: "Component/Element/IntroOneColumnElement",
+    component: IntroOneColumn,
+  },
+  {
+    type: "Component/IntroOneColumnBlock",
+    component: IntroOneColumn,
+  },
+  {
+    type: "Component/Element/ReverseDifferentorPanelElement",
+    component: ReverseDifferentorPanel,
+  },
+  {
+    type: "Component/ReverseDifferentorPanelBlock",
+    component: ReverseDifferentorPanel,
+  },
+];
 
-
-const kajooComponentDictionary = 
-[
-    
-]
-
-export default kajooComponentDictionary
+export default kajooComponentDictionary;
