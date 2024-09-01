00972fde33077c7a72027873e2e36f409d0f2be7
Guvanch G <guvanch.gurbandurdyyev@techguilds.com>
2024-08-31 21:26:33 -0400

    fix link to support children

--- src/components/kajoo/component-library/components/link.tsx
+++ src/components/kajoo/component-library/components/link.tsx
@@ -2,16 +2,22 @@ import NextLink from "next/link";
 import Button from "./button";
 import { FC } from "react";
 
-const Link: FC<any> = ({ href, text = "", ...rest }) => {
-  const hrefUrl = href
-    ? new URL(href.default ?? "/", href.base ?? "https://example.com")
-    : undefined;
+const Link: FC<any> = ({ href, text = "", children, ...rest }) => {
+  let hrefUrl;
+
+  if (typeof href === "string") {
+    hrefUrl = href;
+  } else {
+    hrefUrl = href
+      ? new URL(href.default ?? "/", href.base ?? "https://example.com")
+      : undefined;
+  }
 
   if (!hrefUrl) return <Button text={text} {...rest} />;
 
   return (
     <NextLink href={hrefUrl} {...rest}>
-      {text}
+      {children ? (Array.isArray(children) ? children : [children]) : text}
     </NextLink>
   );
 };
