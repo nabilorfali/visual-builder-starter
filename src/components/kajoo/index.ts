876ba28b241f97c8cf7cee266b986cdd1e9b74c2
Nabil Orfali <41453190+nabilorfali@users.noreply.github.com>
2024-11-15 20:24:30 -0500

    Update index.ts

--- src/components/kajoo/index.ts
+++ src/components/kajoo/index.ts
@@ -1,7 +1,6 @@
-import components from "./components";
-import pages from "./pages";
 
-export const kajooComponentDictionary = [...components, ...pages];
+
+export const kajooComponentDictionary = [];
 
 export default kajooComponentDictionary;
                         
